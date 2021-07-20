62fd9c35c2a9a4ab05e9d79a685d5d86a893b7b9
shehankaushalya <shehanwijesena0000@gmail.com>
2021-07-20 23:46:41 +0530

    added parameters

--- application/views/ImageView.php
+++ application/views/ImageView.php
@@ -4,7 +4,15 @@
 <!-- page body -->
 
 <?php $this->load->view('partials/body_home.php'); ?>
-<?php $this->load->view('partials/nav_progress_details.php'); ?>
+
+<?php 
+ 
+$data['item_code'] = $item_code;
+$data['project_code'] = $project_code;
+$data['location_code'] = $location_code;
+
+$this->load->view('partials/nav_progress_details.php', $data); ?>
+
 <!-- page footer -->
 
 <?php $this->load->view('partials/footer_home'); ?>
