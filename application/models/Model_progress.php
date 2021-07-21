eca091db94101ce5288dda0172c96111e24bd2f3
shehankaushalya <shehanwijesena0000@gmail.com>
2021-07-21 10:37:10 +0530

    changed sql of project image fetch data

--- application/models/Model_progress.php
+++ application/models/Model_progress.php
@@ -186,9 +186,9 @@ class Model_progress extends CI_Model
         // return $query;
     }
 
-    function project_image_fetch_data($item_code, $project_code, $location_code)
+    function project_image_fetch_data($item_code, $project_code, $location_code, $status)
     {
-        $query = $this->db->query("SELECT * FROM progressmaster LEFT JOIN userproject ON progressmaster.ProjectCode=userproject.ProjectCode where userproject.status='checked' && progressmaster.ProjectCode='$project_code' && progressmaster.PhotoItem='$item_code' && progressmaster.LocationCode='$location_code'");
+        $query = $this->db->query("SELECT * FROM progressmaster LEFT JOIN userproject ON progressmaster.ProjectCode=userproject.ProjectCode where userproject.status='checked' && progressmaster.ProjectCode='$project_code' && progressmaster.PhotoItem='$item_code' && progressmaster.LocationCode='$location_code' && progressmaster.Status='$status'");
         return $query;
         // $query = $this->db->query("SELECT DISTINCT progressmaster.ProjectCode, progressmaster.LocationCode, progressmaster.Image, progressmaster.ImgDate, userproject.UserName FROM progressmaster LEFT JOIN userproject ON progressmaster.ProjectCode=userproject.ProjectCode where userproject.status='checked' && progressmaster.ProjectCode='$project_code' && progressmaster.PhotoItem='$item_code' && progressmaster.LocationCode='$location_code'");
         // return $query;
