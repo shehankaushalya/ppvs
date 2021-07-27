63ad7ebd6bf6163773b6f6df2cae31c3f8094cf2
shehankaushalya <shehanwijesena0000@gmail.com>
2021-07-27 15:10:28 +0530

    removed status

--- application/views/partials/CRUD/Progress/E_Edit.php
+++ application/views/partials/CRUD/Progress/E_Edit.php
@@ -117,7 +117,7 @@
                         </div>
 
 
-                        <div class="form-group-inner">
+                        <!-- <div class="form-group-inner">
                             <div class="row">
                                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                                     <label class="login2">Status</label>
@@ -148,7 +148,7 @@
 
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="form-group-inner">
                             <div class="row">
