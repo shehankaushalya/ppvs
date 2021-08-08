fc28a0a10f466b108f9e46fd60ec899d301192c9
shehankaushalya <shehanwijesena0000@gmail.com>
2021-08-09 01:53:25 +0530

    added none

--- application/views/partials/CRUD/Progress/D_Edit.php
+++ application/views/partials/CRUD/Progress/D_Edit.php
@@ -203,11 +203,12 @@
                                     onchange="document.getElementById('D_E_WorkSide').value=this.options[this.selectedIndex].text; 
                                     document.getElementById('idValue').value=this.options[this.selectedIndex].value;">
                                         <option value="-1"></option>
-                                        <option>
-                                                                                             </option>
+                                        
                                                                                              <option>LHS
                                                                                              </option>
                                                                                              <option>RHS
+                                                                                             </option>
+                                                                                             <option>
                                                                                              </option>
                                                                                             
 
