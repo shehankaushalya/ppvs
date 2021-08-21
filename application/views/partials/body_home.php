148db23bd95e8229841e55d60c94c306c41e0ac6
shehankaushalya <shehanwijesena0000@gmail.com>
2021-08-21 20:41:06 +0530

    changed action

--- application/views/partials/body_home.php
+++ application/views/partials/body_home.php
@@ -581,7 +581,7 @@ display:none;
                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                         <div class="basic-login-inner modal-basic-inner">
 
-                                            <form method="post" action="../User/changepassword" class="changepassword">
+                                            <form method="post" action="<?php echo base_url(); ?>index.php/User/changepassword" class="changepassword">
                                                 <div class="form-group-inner">
                                                     <div class="row">
 
